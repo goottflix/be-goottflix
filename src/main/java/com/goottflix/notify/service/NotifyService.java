cbc64cd21b8159426e899cbbe61379f7bcb40f32
nagunwoong <gallawer@naver.com>
2024-09-06 00:58:51 +0900

    알림추가

--- src/main/java/com/goottflix/notify/service/NotifyService.java
+++ src/main/java/com/goottflix/notify/service/NotifyService.java
@@ -4,19 +4,24 @@ package com.goottflix.notify.service;
 import com.goottflix.notify.entity.NotifyEntity;
 import com.goottflix.notify.entity.repository.NotifyMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class NotifyService {
 
-    private NotifyMapper notifyMapper;
+    private final NotifyMapper notifyMapper;
 
 
     public void addMovieUpdate(Long userId, Long movieId) {
+        System.out.println("userId2222222 = " + userId);
         NotifyEntity notify = new NotifyEntity();
         notify.setContent("새로운 영화가 업데이트 되었습니다");
         notify.setUrl("/movie" + movieId);
+        notify.setIsRead(false);
         notify.setNotifyType(NotifyEntity.NotifyType.movieUpdate);
         notify.setUserId(userId);
         notify.setMovieId(movieId);
