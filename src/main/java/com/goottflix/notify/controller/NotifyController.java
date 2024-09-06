4f0e5d91f3c510f1381106d2fef43a3651b668f0
nagunwoong <gallawer@naver.com>
2024-09-06 18:57:00 +0900

    클라이언트에게 알림전송 추가

--- src/main/java/com/goottflix/notify/controller/NotifyController.java
+++ src/main/java/com/goottflix/notify/controller/NotifyController.java
@@ -3,31 +3,40 @@ package com.goottflix.notify.controller;
 
 import com.goottflix.notify.entity.NotifyEntity;
 import com.goottflix.notify.service.NotifyService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/notify")
 public class NotifyController {
 
     private final NotifyService notifyService;
 
+
+    // 영화 업데이트
     @PostMapping("/moiveupdate")
     public ResponseEntity<?> addMovieUpdate(@RequestBody NotifyEntity notifyEntity) {
-
         try {
             notifyService.addMovieUpdate(notifyEntity.getUserId(), notifyEntity.getMovieId());
             return ResponseEntity.ok("업데이트 알림 완료");
-        }catch (Exception e) {
+        } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("업데이트에 실패했습니다");
         }
     }
 
 
+    @GetMapping(value = "/notifyClient", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public void streamNotify(HttpServletResponse response) throws IOException {
+        notifyService.streamNotify(response);
+    }
 }
--- src/main/java/com/goottflix/notify/service/NotifyService.java
+++ src/main/java/com/goottflix/notify/service/NotifyService.java
@@ -3,9 +3,14 @@ package com.goottflix.notify.service;
 
 import com.goottflix.notify.entity.NotifyEntity;
 import com.goottflix.notify.entity.repository.NotifyMapper;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 
 @Slf4j
@@ -14,10 +19,10 @@ import org.springframework.stereotype.Service;
 public class NotifyService {
 
     private final NotifyMapper notifyMapper;
+    private final CopyOnWriteArrayList<HttpServletResponse> clients = new CopyOnWriteArrayList<>();
 
-
+    // 영화 추가 알림 메소드
     public void addMovieUpdate(Long userId, Long movieId) {
-        System.out.println("userId2222222 = " + userId);
         NotifyEntity notify = new NotifyEntity();
         notify.setContent("새로운 영화가 업데이트 되었습니다");
         notify.setUrl("/movie" + movieId);
@@ -29,6 +34,29 @@ public class NotifyService {
 
         // 새로운 알림을 DB에 저장한다잉
         notifyMapper.insertNotify(notify);
+
+        // 업데이트 된 알림을 모든 클라이언트에게 전달 하는 거인듯..?
+        notifyClients("새로운 영화가 업데이트 되었습니다" + movieId);
     }
+
+    // sse 관련한 메서드임
+    public void streamNotify(HttpServletResponse response) throws IOException {
+        response.setContentType(MediaType.TEXT_EVENT_STREAM_VALUE);
+        response.setCharacterEncoding("UTF-8");
+        clients.add(response);
+    }
+
+    //  저장된 모든 클라이언트한테 알림을 전송하는 메서드임
+    public void notifyClients(String content) {
+        clients.forEach(client -> {
+            try {
+                client.getWriter().write("data: " + content + "\n\n");
+                client.getWriter().flush();
+            }catch (IOException e) {
+                clients.remove(client);
+            }
+        });
+    }
+
 
 }
