1b94dcd38fbf2cb4af03dceee29477a2ec0517e2
nagunwoong <gallawer@naver.com>
2024-09-26 12:33:04 +0900

    충돌 해결

--- src/main/java/com/goottflix/notify/service/NotifyService.java
+++ src/main/java/com/goottflix/notify/service/NotifyService.java
@@ -5,7 +5,6 @@ import com.goottflix.notify.entity.NotifyEntity;
 import com.goottflix.notify.entity.repository.NotifyMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
