67b44581bb83dddb245c688b44f4c73c9e5efd49
nagunwoong <gallawer@naver.com>
2024-10-02 01:54:39 +0900

    웹소켓 설정

--- src/main/java/com/goottflix/chat/entity/mapper/ChatRoomMapper.java
+++ src/main/java/com/goottflix/chat/entity/mapper/ChatRoomMapper.java
@@ -8,9 +8,6 @@ import java.util.Map;
 @Mapper
 public interface ChatRoomMapper {
 
-    // 채팅방 생성 메서드
-    void createChatRoom(Map<String, Long> params);
 
-    // 채팅방 ID 조회 메서드
-    Long getChatRoomId(Map<String, Long> params);
+
 }
