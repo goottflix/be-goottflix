f0e4508b53162661a7d05c328f076131713916fd
nagunwoong <gallawer@naver.com>
2024-10-02 11:29:18 +0900

    채팅기능 구현, 테스트 전 버전임

--- src/main/java/com/goottflix/chat/entity/mapper/ChatRoomMapper.java
+++ src/main/java/com/goottflix/chat/entity/mapper/ChatRoomMapper.java
@@ -1,13 +1,19 @@
 package com.goottflix.chat.entity.mapper;
 
 
+import com.goottflix.chat.entity.ChatRoomDTO;
 import org.apache.ibatis.annotations.Mapper;
 
-import java.util.Map;
+import java.util.List;
 
 @Mapper
 public interface ChatRoomMapper {
 
 
+    void insertChatRoom(Long userId1, Long userId2);
+
+    ChatRoomDTO getChatRoomByUserIds(Long userId1, Long userId2);
+
+    List<ChatRoomDTO> getAllChatRooms();
 
 }
--- src/main/java/com/goottflix/chat/entity/mapper/MessageMapper.java
+++ src/main/java/com/goottflix/chat/entity/mapper/MessageMapper.java
@@ -1,4 +1,17 @@
 package com.goottflix.chat.entity.mapper;
 
+import com.goottflix.chat.entity.MessageDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+
+@Mapper
 public interface MessageMapper {
+
+    void insertMessage(Long chatRoomId, Long senderId, String content);
+
+
+    List<MessageDTO> getMessagesByChatRoomId(Long chatRoomId);
+
 }
